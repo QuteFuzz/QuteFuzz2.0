ffa41c75b3438f7f135b5454ff1a23a69837a782
Bennybenassius <bencom2014@live.com>
2025-06-29 18:18:15 +0100

    Slightly modified setup.py file to reflect correct naming of diff_testing library

--- diff_testing/setup.py
+++ diff_testing/setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='QuteFuzz',
-    version='2.0',
+    name='diff_testing',
+    version='0.1',
     packages=find_packages(),
     install_requires=['qiskit', 'pytket', 'cirq', 'pytket-qiskit', 'matplotlib', 'sympy', 'z3-solver', 'pytket-qiskit', 'pytket-quantinuum', 'pygame'],
 )
