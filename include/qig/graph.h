b549e8cce2acc23e11b402cb67659180438c80e0
benny-quantinuum <benny.zong-liu@quantinuum.com>
2025-07-14 17:10:06 +0100

    Code cleanup with some added comments

--- include/qig/graph.h
+++ include/qig/graph.h
@@ -16,12 +16,7 @@ class Graph {
         vertices(_vertices), 
         num_pairs((vertices * (vertices - 1)) / 2),
         graph(vertices, std::vector<int>(vertices, 0)), 
-        shortest_distances(vertices, std::vector<int>(vertices, 0)){
-//             if(vertices < Common::MIN_N_QUBITS_IN_ENTANGLEMENT){
-//                 
-            //     // throw std::runtime_error(ANNOT("Num vertices in graph < " + std::to_string(Common::MIN_N_QUBITS_IN_ENTANGLEMENT)));
-            // }
-        }
+        shortest_distances(vertices, std::vector<int>(vertices, 0)){}
 
         inline std::string get_owner(){return owner;}
 
