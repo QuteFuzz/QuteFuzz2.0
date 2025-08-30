ced2012e2a421ea56db8b1e213c686957028edf0
Ilan Iwumbwe <ikibfm@gmail.com>
2025-08-30 19:53:42 +0100

    moving some code

--- src/ast/node/node.cpp
+++ src/ast/node/node.cpp
@@ -2,6 +2,41 @@
 
 int Node::node_counter = 0;
 
+#ifdef DEBUG
+std::string Node::get_debug_constraint_string() const {
+    if(constraint.has_value()){
+        std::string debug_string;
+
+        for(size_t i = 0; i < constraint.value().rules_size(); i++){
+            unsigned int n_occurances = constraint.value().get_occurances(i);
+            
+            debug_string += std::to_string(constraint.value().get_rule(i)) + " with occurances: " + std::to_string(n_occurances) + " ";
+            
+            if(n_occurances > (unsigned int)WILDCARD_MAX){
+                debug_string += RED("(Cannot be satisfied! Max = " + std::to_string(WILDCARD_MAX) + ")");
+            }
+        }
+
+        return debug_string;
+    
+    } else {
+        return "no constraint";
+    
+    }
+}
+#endif
+
+int Node::get_next_qubit_op_target(){
+    size_t partition_size = qubit_op_target_partition.size();
+
+    if(partition_counter < partition_size){
+        return qubit_op_target_partition[partition_counter++];
+    } else {
+        WARNING("Node " + string + " qubit node target partition info: Counter: " + std::to_string(partition_counter) + ", Size: " + std::to_string(partition_size));
+        return 1;
+    }
+}
+
 std::shared_ptr<Node> Node::find(const U64 _hash) const {
 
     std::shared_ptr<Node> node;
@@ -19,4 +54,84 @@ std::shared_ptr<Node> Node::find(const U64 _hash) const {
     }
 
     return nullptr;
+}
+
+/// @brief Create a random partition of `target` over `n_children`. Final result contains +ve ints
+/// @param target 
+/// @param n_children 
+void Node::make_partition(int target, int n_children){
+
+    if((n_children == 1) || (target == 1)){
+        qubit_op_target_partition = {target};
+    
+    } else if (target == n_children){
+        qubit_op_target_partition = std::vector<int>(n_children, 1);
+
+    } else {
+
+        /*
+            make N-1 random cuts between 1 and T-1
+            ex: 
+                T = 10, N = 4
+                {2, 9, 4}
+        */
+        std::vector<int> cuts;
+
+        for(int i = 0; i < n_children-1; i++){
+            int val = random_int(target-1, 1);
+
+            while(std::find(cuts.begin(), cuts.end(), val) != cuts.end()){
+                val = random_int(target-1, 1);
+            }
+
+            cuts.push_back(val);
+        }
+
+        /*
+            sort the cuts
+            ex:
+                {2, 4, 9}
+        */
+        std::sort(cuts.begin(), cuts.end());
+
+        /*
+            add 0 and T boundaries, then calculate diffs
+            ex:
+                {0, 2, 4, 9, 10}
+                {2, 2, 5, 1} <- result
+        */
+        qubit_op_target_partition.push_back(cuts[0]);
+
+        for(int i = 1; i < n_children-1; i++){
+            qubit_op_target_partition.push_back(cuts[i] - cuts[i-1]);
+        }
+
+        qubit_op_target_partition.push_back(target - cuts[n_children-2]);
+
+    }
+
+#ifdef DEBUG
+    std::cout << "Partition at " << string << std::endl;
+    for(size_t i = 0; i < qubit_op_target_partition.size(); i++){
+        std::cout << qubit_op_target_partition[i] << " ";
+    }
+
+    std::cout << std::endl;
+#endif
+}
+
+/// @brief Make partitions for control flow blocks and branches, adding correct constraints where required
+/// @param target 
+/// @param n_children 
+void make_control_flow_partition(int target, int n_children){
+    make_partition(target, n_children);
+    
+    if(n_children == 1){
+        add_constraint(Common::else_stmt, 0);
+        add_constraint(Common::elif_stmt, 0);
+    } else if (random_int(1)) {
+        add_constraint(Common::else_stmt, 1);
+    } else {
+        add_constraint(Common::elif_stmt, 1);
+    }
 }
