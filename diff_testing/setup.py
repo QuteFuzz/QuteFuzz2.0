f10037d07cb228edd3bb5c348fb3a6f2f9ed2b14
Bennybenassius <126066636+Bennybenassius@users.noreply.github.com>
2025-10-02 21:23:15 +0100

    Revert "Slightly modified setup.py file to reflect correct naming of diff_testing library"
    This reverts commit ffa41c75b3438f7f135b5454ff1a23a69837a782.

--- diff_testing/setup.py
+++ diff_testing/setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='diff_testing',
-    version='0.1',
+    name='QuteFuzz',
+    version='2.0',
     packages=find_packages(),
     install_requires=['qiskit', 'pytket', 'cirq', 'pytket-qiskit', 'matplotlib', 'sympy', 'z3-solver', 'pytket-qiskit', 'pytket-quantinuum', 'pygame'],
 )
